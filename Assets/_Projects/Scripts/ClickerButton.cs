6b87ddd4658abdc323dfe46c7b69e54d8294d9fd
Curtis Teng <curdiskirby@gmail.com>
2025-08-06 20:48:22 -0700

    QOL Update
    - Added BGM
    - Added SFX to clicker button (Berry)
    - Added Status Box
    - Started on adding disasters

--- Assets/_Projects/Scripts/ClickerButton.cs
+++ Assets/_Projects/Scripts/ClickerButton.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class ClickerButton : MonoBehaviour
 {
+    public AudioSource berrySound; // Reference to the audio source for playing sounds
     public void OnClick()
     {
+        berrySound.Play(); // Plays the sound when the button is clicked
         GlobalBerries.totalBerries++; // Increments the static totalBerries variable
     }
 }
