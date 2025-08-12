b9e28fd098068cac56982445eed6df25009af3d6
Curtis Teng <curdiskirby@gmail.com>
2025-08-12 05:09:22 -0700

    Small Changes
    - Ensured the fox animation properly played in the right position regardless of resolution/window size
    - Made it so that you have to click on the actual berry sprite itself

--- Assets/_Projects/Scripts/ClickerButton.cs
+++ Assets/_Projects/Scripts/ClickerButton.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickerButton : MonoBehaviour
 {
     public AudioSource berrySound; // Reference to the audio source for playing sounds
+    public GameObject clickerButton; // Reference to the button GameObject
+
+    void Start()
+    {
+        clickerButton.GetComponent<Button>().image.alphaHitTestMinimumThreshold = 0.01f; // Sets the alpha hit test minimum threshold for the button image
+    }
     public void OnClick()
     {
         berrySound.Play(); // Plays the sound when the button is clicked
