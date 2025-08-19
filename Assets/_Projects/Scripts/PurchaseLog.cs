0068e882c61c5ef522fa345f36941843eabca4a4
Curtis Teng <curdiskirby@gmail.com>
2025-08-19 07:30:38 -0700

    Upgrade Balancing
    - Changed Upgrade Pricing Formula so that upgrade prices are never too close in proximity and are neither too easy nor too hard to get

--- Assets/_Projects/Scripts/PurchaseLog.cs
+++ Assets/_Projects/Scripts/PurchaseLog.cs
@@ -11,7 +11,7 @@ public class PurchaseLog : MonoBehaviour
         playSound.Play(); // Plays the sound when the button is clicked
         GlobalBerries.SubtractBerries(GlobalBerryFarmer.berryFarmerValue); // Deducts the cost from total berries
         StatsDisplayScript.berryFarmerCount += 1; // Increments the count of berry farmers
-        GlobalBerryFarmer.berryFarmerValue = (int)(GlobalBerryFarmer.baseBerryFarmerValue * Mathf.Pow(1.15f, StatsDisplayScript.berryFarmerCount)); // Updates the value of berryFarmerValue based on the count of berry farmers
+        GlobalBerryFarmer.berryFarmerValue = (int)(GlobalBerryFarmer.baseBerryFarmerValue * Mathf.Pow(1.13f, StatsDisplayScript.berryFarmerCount) * (1 + StatsDisplayScript.berryFarmerCount * 0.1f) + (StatsDisplayScript.berryFarmerCount * 25)); // Updates the value of berryFarmerValue based on the count of berry farmers
         GlobalBerryFarmer.turnOffButton = true; // Sets the static variable to true to turn off the button
         StatsDisplayScript.berriesPerSecond += 1; // Increases the berries produced per second by 1
     }
@@ -21,7 +21,7 @@ public class PurchaseLog : MonoBehaviour
         playSound.Play(); // Plays the sound when the button is clicked
         GlobalBerries.SubtractBerries(GlobalBerryBush.berryBushValue); // Deducts the cost from total berries
         StatsDisplayScript.berryBushCount += 1; // Increments the count of berry bushes
-        GlobalBerryBush.berryBushValue = (int)(GlobalBerryBush.baseBerryBushValue * Mathf.Pow(1.15f, StatsDisplayScript.berryBushCount)); // Updates the value of berryBushValue based on the count of berry bushes
+        GlobalBerryBush.berryBushValue = (int)(GlobalBerryBush.baseBerryBushValue * Mathf.Pow(1.20f, StatsDisplayScript.berryBushCount) * (1 + StatsDisplayScript.berryBushCount * 0.25f) + (StatsDisplayScript.berryBushCount * 25)); // Updates the value of berryBushValue based on the count of berry bushes
         GlobalBerryBush.turnOffButton = true; // Sets the static variable to true to turn off the button
         StatsDisplayScript.berriesPerClick += 1; // Increases the berries produced per click by 1
     }
